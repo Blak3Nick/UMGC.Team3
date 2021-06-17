a2913a3c70eb5d5821f9771817b48473140818fb
Blak3nick <blaken2021@gmail.com>
2021-06-17 17:18:16 -0400

    Added in a method to update the Firebase with the completed sets.

---
+++ app/src/main/java/com/example/umgcteam3/UpdateDatabase.java
@@ -0,0 +1,93 @@
+package com.example.umgcteam3;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateDatabase extends AsyncTask<Void, Void, String> {
+    Context context;
+    int set;
+    int reps;
+    int rpe;
+    int weight;
+    int day_number;
+    String user_id;
+    String exercise_name;
+    String exDate;
+    String setNumber;
+    boolean readyToIncrease = false;
+    public UpdateDatabase(Context context, int set, int reps, int rpe, int weight, int day_number,  String exercise_name, String exDate, boolean readyToIncrease) {
+        this.context = context;
+        this.set = set;
+        this.reps = reps;
+        this.rpe = rpe;
+        this.weight = weight;
+        this.day_number = day_number;
+        this.exercise_name = exercise_name;
+        this.exDate = exDate;
+        this.setNumber = "Set" + set;
+        this.readyToIncrease = readyToIncrease;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        user_id = fAuth.getCurrentUser().getUid();
+        Map<String, Object> completedSet = new HashMap<>();
+        String date = FieldValue.serverTimestamp().toString();
+        System.out.println(date);
+        completedSet.put("DateCompleted", FieldValue.serverTimestamp());
+        completedSet.put("SetNumber", set);
+        completedSet.put("Reps", reps);
+        completedSet.put("WeightUsed", weight);
+        completedSet.put("RPE", rpe);
+        completedSet.put("DayNumber", day_number);
+        completedSet.put("Increase?", readyToIncrease);
+
+        db.collection("users").document(user_id).collection("CompletedWorkoutsRevised").document(exercise_name).collection("AllSets").document(exDate).collection(setNumber)
+                .document(setNumber+"Details").set(completedSet, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("SUCCESS", "Written to the database");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("FAILURE", e.getMessage() );
+            }
+        });
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {    }
+
+    @Override
+    protected void onPostExecute(String result) {    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
