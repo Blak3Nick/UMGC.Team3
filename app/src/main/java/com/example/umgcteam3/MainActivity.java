12f3d603b6768488d9af26437b97622e43ed0bbb
MalloryJaneCS <67796709+malloryjanecs@users.noreply.github.com>
2021-07-01 12:27:21 -0400

    Fixed logout fatal error if() statement documentSnapshot.exists() crashes program when logging out check if documentSnapshot is null before calling .exists()

--- app/src/main/java/com/example/umgcteam3/MainActivity.java
+++ app/src/main/java/com/example/umgcteam3/MainActivity.java
@@ -15,8 +15,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -27,6 +30,8 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 import javax.annotation.Nullable;
 
@@ -78,9 +83,13 @@ public class MainActivity extends AppCompatActivity {
         resendCode = findViewById(R.id.resendCode);
         verifyMsg = findViewById(R.id.verifyMsg);
 
+        try{
+            userId = fAuth.getCurrentUser().getUid();
+            user = fAuth.getCurrentUser();
+        } catch (NullPointerException e){
+            finish();
+        }
 
-        userId = fAuth.getCurrentUser().getUid();
-        user = fAuth.getCurrentUser();
 
         if(!user.isEmailVerified()){
             verifyMsg.setVisibility(View.VISIBLE);
