1d6b380f7e5dbbcf0ab361dc31881f949afb2327
lcigarruista <lourdes.cigarruista@gmail.com>
2021-07-02 12:01:01 -0400

    Changes to this file to add ux images/buttons/layouts.

--- app/src/main/java/com/example/umgcteam3/RegisterActivity.java
+++ app/src/main/java/com/example/umgcteam3/RegisterActivity.java
@@ -85,7 +85,7 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
-                progressBar.setVisibility(View.VISIBLE);
+                //progressBar.setVisibility(View.VISIBLE);
 
                 // register the user in firebase
 
