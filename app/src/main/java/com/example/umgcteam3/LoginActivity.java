f504e1375e15e0e5e8bc7b4011f4922514a6095d
Blak3nick <blaken2021@gmail.com>
2021-07-04 13:37:00 -0400

    Added an alert dialog for when the email is already in use when a user attempts to create an account.

--- app/src/main/java/com/example/umgcteam3/LoginActivity.java
+++ app/src/main/java/com/example/umgcteam3/LoginActivity.java
@@ -22,11 +22,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
