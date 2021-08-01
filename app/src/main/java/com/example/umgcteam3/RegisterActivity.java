13f5fd90b48697e59f43001fc9f11e542761be66
MalloryJaneCS <67796709+malloryjanecs@users.noreply.github.com>
2021-08-01 00:07:43 -0400

    Fixed typo for confirmPassword field; now correctly checks that the two password fields match

--- app/src/main/java/com/example/umgcteam3/RegisterActivity.java
+++ app/src/main/java/com/example/umgcteam3/RegisterActivity.java
@@ -71,7 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
                 System.out.println("Creating account...");;
                 final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
-                String confirmPassword = mPassword.getText().toString().trim();
+                String confirmPassword = mConfirmPassword.getText().toString().trim();
                 final String fullName = mFullName.getText().toString();
                 final String phone    = mPhone.getText().toString();
 
